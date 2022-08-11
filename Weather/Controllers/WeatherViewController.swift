5e5348389f984e5905dd981c763ad69aa0796d3b
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-08-11 18:05:08 +0300

    create design mvc grouping

---
+++ Weather/Controllers/WeatherViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherViewController.swift
+//  Weather
+//
+//  Created by Алексей Гуляев on 11.08.2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
